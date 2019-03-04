3c854810e0dc1c2bf84297cacd78b6d708698da4
Telkamp, Philip <philip.telkamp@mines.sdsmt.edu>
2019-03-03 18:10:51 -0700

    Parsing an entire directory and displaying all files contained within that directory

--- genDoxy.py
+++ genDoxy.py
@@ -6,11 +6,29 @@ def parseDir(filePath):
 		# print("Dir Path: ", dirPath);
 		# print("Dir Name: ", dirNames);
 		# print("File Names: ", fileNames);
+		for fileName in fileNames:
+			parseFile(dirPath + "/" + fileName);
+
+
+def parseFile(path):
+	print("Parsing file: ", path);
+
+
 
 def generateDoxygen():
 	path = "";
-	while(path != "q"):
+	while( path != "q" ):
 		path = input("Please enter absolute path to target directory/file (Enter q to quit): ");
-		parseDir(path);
+		if( os.path.isfile(path) ):
+			parseFile(path);
+		elif(os.path.isdir(path) ):
+			if(path[-1] == "/"):
+				parseDir(path[:-1])
+			else:
+				parseDir(path)
+		elif( path == "q" ):
+			print("Exiting...")
+		else:
+			print("Error finding/opening file or directory");
 
 generateDoxygen();
